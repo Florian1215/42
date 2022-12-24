5aaea4e7b9b19f20463e75ea45eb572179bc9c48
Florian1215 <florian.guiramand@protonmail.com>
2022-12-24 19:30:46 +0100

    GNL

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -12,7 +12,7 @@
 
 #include "get_next_line.h"
 
-int	ft_find_nl(t_list *lst, int one_lst)
+static int	find_nl(t_list *lst, int one_lst)
 {
 	int	size;
 	int	i;
@@ -35,14 +35,55 @@ int	ft_find_nl(t_list *lst, int one_lst)
 	return (-1);
 }
 
-char	*ft_add_res(t_list **lst)
+static int  lstadd_back(t_list **lst, char *buff)
+{
+    t_list	*new_lst;
+
+    new_lst = malloc(sizeof(t_list));
+    if (!new_lst)
+        return (free(buff), 1);
+    new_lst->buff = buff;
+    new_lst->next = NULL;
+    if (!*lst)
+        *lst = new_lst;
+    else
+        lstlast(*lst)->next = new_lst;
+    return (0);
+}
+
+static void	lstrm_first(t_list **lst)
+{
+    t_list	*tmp;
+    int		st;
+    int		i;
+
+    if (!*lst)
+        return ;
+    st = find_nl(*lst, 1);
+    if (st == -1 || !(*lst)->buff[st])
+    {
+        tmp = *lst;
+        *lst = (*lst)->next;
+        free(tmp->buff);
+        free(tmp);
+    }
+    else
+    {
+        i = 0;
+        while ((*lst)->buff[st])
+            (*lst)->buff[i++] = (*lst)->buff[st++];
+        (*lst)->buff[i] = '\0';
+    }
+}
+
+static char	*get_line(t_list **lst)
 {
 	char	*res;
 	int		i;
 	int		j;
 	int		loop;
 
-	res = malloc(sizeof(char) * (ft_find_nl(*lst, 0) + 1));
+	res = malloc(sizeof(char) * (find_nl(*lst, 0) + 1));
 	if (!res)
 		return (0);
 	i = 0;
@@ -56,7 +97,7 @@ char	*ft_add_res(t_list **lst)
 			if (res[i++] == '\n')
 				loop = 0;
 		}
-		ft_lstrm_first(lst);
+		lstrm_first(lst);
 	}
 	return (res[i] = '\0', res);
 }
@@ -70,19 +111,17 @@ char	*get_next_line(int fd)
 	if (fd == -1 || BUFFER_SIZE <= 0)
 		return (0);
 	read_bytes = 1;
-	while (ft_find_nl(ft_lstlast(lst), 1) == -1 && read_bytes)
+	while (find_nl(lstlast(lst), 1) == -1 && read_bytes)
 	{
 		buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 		if (!buff)
-			return (0);
+			return (NULL);
 		read_bytes = read(fd, buff, BUFFER_SIZE);
 		if ((!lst && !read_bytes) || read_bytes < 0)
-		{
-			free(buff);
-			return (0);
-		}
+			return (free(buff), NULL);
 		buff[read_bytes] = '\0';
-		ft_lstadd_back(&lst, ft_lstnew(buff));
+        if (lstadd_back(&lst, buff))
+            return (NULL);
 	}
-	return (ft_add_res(&lst));
+	return (get_line(&lst));
 }
